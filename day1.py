1b7b6710cb12fc60ab46612d6c3f7e680e34c32c
Matthew Kuo <matthewkuo92@gmail.com>
2015-12-11 20:49:34 -0800

    Day 1 part 2

--- day1.py
+++ day1.py
@@ -5,6 +5,14 @@ def solve(content):
     step = lambda acc, curr: acc + (1 if curr == '(' else -1)
     return functools.reduce(step, content, 0)
 
+def solve2(content):
+    floor = 0
+    for i, instruction in enumerate(content):
+        floor += (1 if instruction == '(' else -1)
+        if floor == -1:
+            return i + 1
+
 if __name__ == '__main__':
     content = inputgetter.get_input()
-    print(solve(content))
+    print("Floor #: {}".format(solve(content)))
+    print("First reached basement at instruction #: {}".format(solve2(content)))
