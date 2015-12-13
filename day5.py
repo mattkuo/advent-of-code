f1d38882a2666958cf6a579fbc56fac4e92b183b
Matthew Kuo <matthewkuo92@gmail.com>
2015-12-13 01:05:52 -0800

    day 5 part 2

--- day5.py
+++ day5.py
@@ -1,10 +1,14 @@
 import inputgetter
+import re
 
 VOWELS = 'aeiou'
 BANNED = ['ab', 'cd', 'pq', 'xy']
 
 def is_nice(string):
 	return has_sufficient_vowels(string) and is_clean(string) and has_repeat(string)
+
+def is_nice2(string):
+	return has_pairs(string) and has_burger_pairs(string)
 
 def has_sufficient_vowels(string):
 	count = 0
@@ -27,11 +31,26 @@ def has_repeat(string):
 		else:
 			current_char = char
 
+def has_pairs(string):
+	for x in range(len(string) - 1):
+		if string.count(string[x:x+2], x + 2) >= 1:
+			return True
+	return False
+
+def has_burger_pairs(string):
+	return re.search(r"(\w)\w\1", string) is not None
+
 if __name__ == '__main__':
 	content = inputgetter.get_input().split("\n")
 	count = 0
+	count2 = 0
+	
 	for string in content:
 		if is_nice(string):
 			count += 1
+		if is_nice2(string):
+			count2 += 1
 
 	print("There are {} nice words".format(count))
+	print("There are {} nice words".format(count2))
+
